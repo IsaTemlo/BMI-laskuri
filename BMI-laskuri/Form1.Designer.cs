fcfe7ed58bc5cea6491f71a01e77f65d2d1eaf90
Temlo <89012075+IsaTemlo@users.noreply.github.com>
2021-09-09 20:19:06 +0300

    Add project files.

---
+++ BMI-laskuri/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace BMI_laskuri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PainoTB = new System.Windows.Forms.TextBox();
+            this.PituusTB = new System.Windows.Forms.TextBox();
+            this.BMIBT = new System.Windows.Forms.Button();
+            this.KuvausLB = new System.Windows.Forms.Label();
+            this.BmiLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna paino";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Anna pituus";
+            // 
+            // PainoTB
+            // 
+            this.PainoTB.Location = new System.Drawing.Point(234, 64);
+            this.PainoTB.Name = "PainoTB";
+            this.PainoTB.Size = new System.Drawing.Size(100, 35);
+            this.PainoTB.TabIndex = 2;
+            // 
+            // PituusTB
+            // 
+            this.PituusTB.Location = new System.Drawing.Point(234, 141);
+            this.PituusTB.Name = "PituusTB";
+            this.PituusTB.Size = new System.Drawing.Size(100, 35);
+            this.PituusTB.TabIndex = 3;
+            // 
+            // BMIBT
+            // 
+            this.BMIBT.Location = new System.Drawing.Point(53, 233);
+            this.BMIBT.Name = "BMIBT";
+            this.BMIBT.Size = new System.Drawing.Size(281, 47);
+            this.BMIBT.TabIndex = 4;
+            this.BMIBT.Text = "Laske painoindeksi";
+            this.BMIBT.UseVisualStyleBackColor = true;
+            this.BMIBT.Click += new System.EventHandler(this.BMIBT_Click);
+            // 
+            // KuvausLB
+            // 
+            this.KuvausLB.AutoSize = true;
+            this.KuvausLB.Location = new System.Drawing.Point(48, 366);
+            this.KuvausLB.Name = "KuvausLB";
+            this.KuvausLB.Size = new System.Drawing.Size(87, 29);
+            this.KuvausLB.TabIndex = 5;
+            this.KuvausLB.Text = "kuvaus";
+            this.KuvausLB.Visible = false;
+            // 
+            // BmiLB
+            // 
+            this.BmiLB.AutoSize = true;
+            this.BmiLB.Location = new System.Drawing.Point(53, 316);
+            this.BmiLB.Name = "BmiLB";
+            this.BmiLB.Size = new System.Drawing.Size(53, 29);
+            this.BmiLB.TabIndex = 6;
+            this.BmiLB.Text = "bmi";
+            this.BmiLB.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 426);
+            this.Controls.Add(this.BmiLB);
+            this.Controls.Add(this.KuvausLB);
+            this.Controls.Add(this.BMIBT);
+            this.Controls.Add(this.PituusTB);
+            this.Controls.Add(this.PainoTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PainoTB;
+        private System.Windows.Forms.TextBox PituusTB;
+        private System.Windows.Forms.Button BMIBT;
+        private System.Windows.Forms.Label KuvausLB;
+        private System.Windows.Forms.Label BmiLB;
+    }
+}
+
